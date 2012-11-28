3928be309f272cfbe0ed4135dfb69b1e1104f835
Edwin Steele <edwin-lists@wordspeak.org>
2012-11-28 17:39:38 +1100

    Initial pass at the non-rendering bits of the model

--- vcapp/models.py
+++ vcapp/models.py
@@ -1,4 +1,14 @@
+"""
+These models can be more easily understood in reference to the old-style paper
+ timetable where there are a list of stations down one side, trips are columns
+ with each non-empty cell being a tripstop (relating a Station to a point in
+ time). A segment is a pair of vertically adjacent non-empty cells (TripStops)
+
+The timetable itself is a collection of trips on a single Line (usually)
+"""
+
 from django.db import models
+import geometry
 
 class Station(models.Model):
     station_id = models.IntegerField(primary_key=True)
@@ -7,7 +17,22 @@ class Station(models.Model):
     lat = models.FloatField()
 
     def __unicode__(self):
-        return u"%s" % self.id
+        return u"%s - lon: %s (E-W) lat: %s (N-S)" % \
+               (self.station_name, self.lon, self.lat)
+
+    def short_name(self):
+        # Removes the trailing " Station", at least for now.
+        return u"%s" % (self.station_name.rsplit(" ", 1)[0])
+
+    # May be better suited to a point in space time class
+    def distance_from(self, location):
+        return geometry.line_magnitude(x1=self.lon,
+            y1=self.lat,
+            x2=location.lon,
+            y2=location.lat)
+
+    def find_closest_segment(self):
+        pass
 
 
 class Line(models.Model):
@@ -15,48 +40,116 @@ class Line(models.Model):
     line_name = models.CharField(max_length=50)
 
     def __unicode__(self):
-        return u"%s" % self.id
+        return u"%s (id %s)" % (self.line_name, self.line_id)
 
 
 class Trip(models.Model):
+    """
+    A Trip is a column in a traditional timetable. It is a list of TripStops
+    """
     TIMETABLE_TYPE_CHOICES = (
         ('WD', 'Weekday'),
         ('WE', 'Weekend'),
     )
     trip_id = models.IntegerField(primary_key=True)
     timetable_type = models.CharField(max_length=2, choices=TIMETABLE_TYPE_CHOICES)
-    line_id = models.ForeignKey('Line')
+    line = models.ForeignKey('Line')
 
     def __unicode__(self):
-        return u"%s" % self.id
+        return u"Trip (id:%s) from %s to %s, on line %s" % \
+               (self.trip_id,
+                self.get_segments()[0].station,
+                self.get_segments()[-1].station,
+                self.line.line_id)
+
+    def get_segments(self):
+        return self.trip_set.all()
+
+    def get_trip_distance(self):
+        return sum([segment.distance() for segment in self.get_segments()])
+
+    # FIXME - Do we really need these methods to be in hours? why not just datetime.time?
+    def get_start_hour(self):
+        return self.get_segments()[0].departure_tripstop.departure_time.hour
+
+    def get_end_hour(self):
+        return self.get_segments()[-1].arrival_tripstop.arrival_time.hour
 
 
 class TripStop(models.Model):
     """
+    A TripStop is a point in space-time on a trip. It corresponds to a cell in a
+     traditional timetable, where a trip intersects with a station.
+
     key should be tripId + stationId. A station can't appear more than once
     on a particular trip (what about city circle?)
     """
     tripstop_id = models.IntegerField(primary_key=True)
     departure_time = models.TimeField()
-    trip_id = models.ForeignKey('Trip')
-    station_id = models.ForeignKey('Station')
+    trip = models.ForeignKey('Trip')
+    station = models.ForeignKey('Station')
 
     def __unicode__(self):
         return u"%s" % self.id
 
 
 class Segment(models.Model):
+    """
+    Note that because we only store departure time, we don't know how long a
+    train actually waits at a station. close enough for the moment
+    """
     segment_id = models.IntegerField(primary_key=True)
-    departure_tripstop_id = models.ForeignKey('TripStop', related_name='departures')
-    arrival_tripstop_id = models.ForeignKey('TripStop', related_name='arrivals')
+    # FIXME - do I really need to create a backwards relation for these FKs?
+    # https://docs.djangoproject.com/en/dev/topics/db/queries/#backwards-related-objects
+    departure_tripstop = models.ForeignKey('TripStop', related_name='departure_point')
+    arrival_tripstop = models.ForeignKey('TripStop', related_name='arrival_point')
+    # TODO - work out how interchange stations fit here. Are they an on-the-fly
+    #  segment? Or are they not really a segment at all?
+    trip = models.ForeignKey('Trip')
+    # The old model had line as an attribute. Is it important?
 
     def __unicode__(self):
-        return u"%s" % self.id
+        return u"Segment [%s] from %s (%s) to %s (%s)" % \
+               (self.segment_id,
+                self.departure_tripstop.station,
+                self.departure_tripstop.departure_time,
+                self.arrival_tripstop.station,
+                self.arrival_tripstop.departure_time
+            )
+
+    def get_trip_id(self):
+        # arrival and departure trip id should be the same, perhaps with the
+        #  exception of interchange segments
+        assert self.departure_tripstop.trip_id == self.arrival_tripstop.trip_id
+        return self.trip.trip_id
+
+    def get_line_id(self):
+        # arrival and departure line id should be the same, perhaps with the
+        #  exception of interchange segments
+        assert self.departure_tripstop.trip.line.line_id == \
+               self.arrival_tripstop.trip.line.line_id
+        return self.trip.line.line_id
+
+    def get_departure_point_name(self):
+        return "%s on trip %s" % (self.departure_tripstop.station_id.station_name,
+            self.get_trip_id())
+
+    def get_arrival_point_name(self):
+        return "%s on trip %s" % (self.arrival_tripstop.station_id.station_name,
+            self.get_trip_id())
+
+    def add_as_digraph_edge(self, dg, ignore_lines):
+        # TODO - implementation in the old class
+        pass
+
+    def segment_length(self):
+        return self.departure_tripstop.station.distance_from(
+                self.arrival_tripstop.station)
 
 
 class InterchangeStation(models.Model):
-    line_id = models.ForeignKey("Line")
-    station_id = models.ForeignKey("Station")
+    line = models.ForeignKey("Line")
+    station = models.ForeignKey("Station")
 
     def __unicode__(self):
         return u"%s" % self.id
